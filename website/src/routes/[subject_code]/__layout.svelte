f24d9f43ff827b6e0c4c5e4e3318323624c7b9ac
gtm-nayan <gtmnayan@gmail.com>
2022-02-16 09:29:59 +0545

    strict mode

--- website/src/routes/[subject_code]/__layout.svelte
+++ website/src/routes/[subject_code]/__layout.svelte
@@ -1,9 +1,12 @@
 <script context="module" lang="ts">
 	import subjects from '$lib/data/subjects.json';
+	import type { SubjectCode } from '$lib/utils/types';
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async ({ params }) => {
-		if (!subjects[params.subject_code])
+		const subject_code = params.subject_code as SubjectCode;
+
+		if (!subjects[subject_code])
 			return {
 				status: 404,
 				error: new Error('No matching subject found for that code.'),
