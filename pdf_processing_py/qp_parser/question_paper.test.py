745875c0bb8dcc10e66150804c8eec815adbe234
gtm-nayan <gtmnayan@gmail.com>
2022-02-21 11:36:41 +0545

    Add back len test

--- pdf_processing_py/qp_parser/question_paper.test.py
+++ pdf_processing_py/qp_parser/question_paper.test.py
@@ -16,10 +16,11 @@ class TestSuite(unittest.TestCase):
     def test_extraction(self):
         qp = QuestionPaper("9701_m16_qp_12.pdf")
         questions = qp.extract_questions()
-        # self.assertEqual(len(questions), 40)
-
+        num = 0
         for q in questions:
             print(q)
+            num += 1
+        self.assertEqual(num, 40)
 
 
 if __name__ == "__main__":
