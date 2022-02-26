de2d9c145fa11c2003ab52278cea3a2826b17058
gtm-nayan <gtmnayan@gmail.com>
2022-02-26 16:49:32 +0545

    Remove "test" from test functions

--- pdf_processing_py/src/question_paper.test.py
+++ pdf_processing_py/src/question_paper.test.py
@@ -3,7 +3,7 @@ from question_paper import QuestionPaper
 
 
 class TestSuite(unittest.TestCase):
-    def test_extraction(self):
+    def extraction(self):
         qp = QuestionPaper("9701_m16_qp_12.pdf")
         questions = qp.extract_questions()
         num = 0
