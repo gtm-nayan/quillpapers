ebb3e49ef07970b91e3c0a3c420242dffee3aca2
gtm-nayan <gtmnayan@gmail.com>
2022-04-10 12:27:56 +0545

    Use native dialog

--- website/src/routes/[subject_code]/casual/_components/ErrorModal.svelte
+++ website/src/routes/[subject_code]/casual/_components/ErrorModal.svelte
@@ -1,82 +1,76 @@
 <script lang="ts">
 	import subjects from '$lib/data/subjects.json';
 	import { zero_two } from '$lib/utils/pdf_url_gen';
-	import subject_code from '../../_subject_code_store';
 	import { QuestionErrorType, type Question } from '$lib/utils/types';
 	import { faFlag } from '@fortawesome/free-solid-svg-icons';
 	import { getContext } from 'svelte';
 	import { FaSvg, Icon } from 'svelte-yafal';
+	import { noop } from 'svelte/internal';
 	import type { Writable } from 'svelte/store';
+	import subject_code from '../../_subject_code_store';
 
 	const current_question: Writable<Question> = getContext('current_question');
 
 	async function handle_submit() {
-		try {
-			await fetch(`/${$subject_code}/casual.json`, {
+		console.log(
+			fetch(`/${$subject_code}/casual.json`, {
 				method: 'POST',
 				body: JSON.stringify({
 					...$current_question,
 					error_type,
 					topic_suggestion,
 				}),
-			});
-		} finally {
-			show_modal = false;
-		}
+			}).catch(noop)
+		);
 	}
 
 	let error_type: QuestionErrorType = QuestionErrorType.BAD_CROPPING;
 
 	let topic_suggestion = '1';
-	let show_modal = false;
+	let dialog: HTMLDialogElement;
+
+	function modal(show: boolean) {
+		// @ts-expect-error
+		dialog[show ? 'showModal' : 'close']?.();
+	}
 </script>
 
 <span>
-	{#if show_modal}
-		<div class="backdrop">
-			<div class="content">
-				<h4>
-					{$current_question.subject_code}
-					{$current_question.series}{zero_two($current_question.exam_year)}
-					{$current_question.paper_variant}
-					QN: {zero_two($current_question.question_number)}
-				</h4>
-				<form on:submit|preventDefault={handle_submit}>
-					<fieldset>
-						<legend>Error type</legend>
-						<select bind:value={error_type}>
-							<option value={QuestionErrorType.BAD_CROPPING}>
-								Bad cropping
-							</option>
-							<option value={QuestionErrorType.WRONG_ANSWER}>
-								Wrong answer
-							</option>
-							<option value={QuestionErrorType.WRONG_TOPIC}>Wrong topic</option>
-						</select>
-					</fieldset>
+	<dialog bind:this={dialog}>
+		<h4>
+			{$current_question.subject_code}
+			{$current_question.series}{zero_two($current_question.exam_year)}
+			{$current_question.paper_variant}
+			QN: {zero_two($current_question.question_number)}
+		</h4>
+		<form on:submit={handle_submit} method="dialog">
+			<fieldset>
+				<legend>Error type</legend>
+				<select bind:value={error_type}>
+					<option value={QuestionErrorType.BAD_CROPPING}>Bad cropping</option>
+					<option value={QuestionErrorType.WRONG_ANSWER}>Wrong answer</option>
+					<option value={QuestionErrorType.WRONG_TOPIC}>Wrong topic</option>
+				</select>
+			</fieldset>
 
-					{#if error_type === QuestionErrorType.WRONG_TOPIC}
-						<fieldset>
-							<legend>Which topic would be better for this question?</legend>
-							<select bind:value={topic_suggestion}>
-								{#each Object.entries(subjects[$subject_code].topics) as [topic_number, { title }]}
-									<option value={topic_number}>{title}</option>
-								{/each}
-							</select>
-						</fieldset>
-					{/if}
-					<button type="submit">Submit</button>
-					<button type="button" on:click={() => (show_modal = false)}>
-						Cancel
-					</button>
-				</form>
-			</div>
-		</div>
-	{/if}
+			{#if error_type === QuestionErrorType.WRONG_TOPIC}
+				<fieldset>
+					<legend>Which topic would be better for this question?</legend>
+					<select bind:value={topic_suggestion}>
+						{#each Object.entries(subjects[$subject_code].topics) as [topic_number, { title }]}
+							<option value={topic_number}>{title}</option>
+						{/each}
+					</select>
+				</fieldset>
+			{/if}
+			<button type="submit">Submit</button>
+			<button type="button" on:click={() => modal(false)}>Cancel</button>
+		</form>
+	</dialog>
 
 	<button
 		type="button"
-		on:click={() => (show_modal = true)}
+		on:click={() => modal(true)}
 		style:color="var(--gray-7)"
 	>
 		<FaSvg><Icon icon={faFlag} /></FaSvg>
@@ -85,24 +79,22 @@
 </span>
 
 <style>
-	.backdrop {
-		position: fixed;
+	::backdrop {
 		background-color: rgb(0 0 0 / 0.5);
-		inset: 0;
-		z-index: var(--layer-important);
-		display: grid;
-		place-items: center;
 	}
 
-	.content {
+	dialog {
 		background-color: white;
 		padding: 1rem;
 		border-radius: 0.5rem;
-		width: minmax(90%, var(--size-content-3));
+		border: none;
+		width: 90%;
+		max-width: 80ch;
 	}
 
 	fieldset {
 		margin: 2rem;
+		border-radius: var(--radius-2);
 	}
 
 	legend {
